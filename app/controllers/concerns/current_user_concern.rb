77862e9c96b7b4fce57968ac23617580f83b8aa7
Kalib Hackin <nagui08@hotmail.com>
2020-03-12 10:15:49 -0500

    other fix

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,7 +6,8 @@ module CurrentUserConcern
   end
 
   def set_current_user
-    return unless session[:user_id]
+    return unless session[:user_id]?
+    
     @current_user = User.find(session[:user_id])
   end
 end
