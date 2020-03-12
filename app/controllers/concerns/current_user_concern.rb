eeb296f9ded432c0c57ec786ea41dcededb3b973
Kalib Hackin <nagui08@hotmail.com>
2020-03-12 10:12:07 -0500

    more fixes

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -7,7 +7,6 @@ module CurrentUserConcern
 
   def set_current_user
     return unless session[:user_id]
-    
     @current_user = User.find(session[:user_id])
   end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,8 @@ class RegistrationsController < ApplicationController
       last_name: params['user']['last_name'],
       email: params['user']['email'],
       password: params['user']['password'],
-      password_confirmation: params['user']['password_confirmation'])
+      password_confirmation: params['user']['password_confirmation']
+    )
 
     if user
       session[:user_id] = user.id
