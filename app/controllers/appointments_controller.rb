16d8fc6c958f717a069084bfd0e34e2005187fe1
Kalib Hackin <nagui08@hotmail.com>
2020-03-17 11:20:30 -0500

    added appointments controller and routes

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,26 @@
+class AppointmentsController < ApplicationController
+  def create
+    appointment = Appointment.create!(
+      user_id: params['user_id'],
+      car_id: params['car_id'],
+      date: params['date'],
+      city: params['city']
+    )
+
+    if appointment
+      render json: {
+        appointment: appointment
+      }
+    else
+      render json: { status: 500 }
+    end
+  end
+
+  def show
+    appointments = Appointment.where(user_id: params[:id])
+
+    render json: {
+      appointments: appointments
+    }
+  end
+end
