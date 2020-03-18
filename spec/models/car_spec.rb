9f672864dd93c37ebdbd9626a773763f3f5a002d
Kalib Hackin <nagui08@hotmail.com>
2020-03-18 15:12:26 -0500

    tests are done

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:price) }
+  it { should validate_presence_of(:car_type) }
+  it { should validate_presence_of(:image) }
 end
