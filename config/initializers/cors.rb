8bfe5945977136b3560b1dfe76cada5c1026d3d5
Kalib Hackin <nagui08@hotmail.com>
2020-03-22 21:07:16 -0500

    modified link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
   
   allow do
-    origins 'https://try-lexus.web.app/'
+    origins 'https://try-lexus.web.app'
 
     resource '*',
       headers: :any,
